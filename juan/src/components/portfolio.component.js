6d0e3dcf8127c99d258e856588a6675ba95e8b41
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-08-06 13:09:05 -0700

    removing shit i didnt add

--- juan/src/components/portfolio.component.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-export default class portfolio extends Component {
-  render() {
-    return <div>This is the portfolio</div>;
-  }
-}
